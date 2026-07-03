0396076987ee5512269e22a77e5f335fbad99084
thomastruffin <95939163+thomastruffin@users.noreply.github.com>
2026-07-03 10:18:18 +0000

    test 7

--- script/main.js
+++ script/main.js
@@ -1,23 +1,6 @@
 import Link from "./const"
 function generateSpecies(event){
-    event.preventDefault()
+    event.preventDefault;
     console.log(event);
-
-    /** return fetch(Link.animal_API).then(Response => 
-    {
-        console.log(Response);
-        if (!reponse.ok) {
-            throw new Error(`Statut de réponse : ${reponse.status}`);
-        }
-        return Response.json;
-    }
-).then(json =>
-{
-    const speciesname = json.city;
-    return document.getElementById("fakemon_species").innerHTML = speciesname;
-}
-).catch(error => {
-    throw error;
-}) **/
 }
 document.getElementById("genrateRandomSpecies").addEventListener("click", event => generateSpecies);
