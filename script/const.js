9a263d65d57305d022844e4fc4c35f7291ec669a
thomastruffin <95939163+thomastruffin@users.noreply.github.com>
2026-07-03 12:26:05 +0000

    html_change

--- script/const.js
+++ script/const.js
@@ -3,6 +3,82 @@ const Link = {
 myth_API :"https://mythapi.onrender.com/Myths/all"
 }
 
+const fakemon_type = [
+    {
+        "type":"Bug",
+        "color":"#91A014"
+    },
+    {
+        "type":"Electric",
+        "color":"#FCBA18"
+    },
+    {
+        "type":"Fire",
+        "Color":"#C72000",
+    },
+    {
+        "type":"Grass",
+        "color":"#76C337"
+    },
+    {
+        "type":"Normal",
+        "color":"#BCB9AC"
+    },
+    {
+        "type":"Rock",
+        "color":"#9E863C"
+    },
+    {
+        "type":"Dark",
+        "color":"#3E2F25"
+    },
+    {
+        "type":"Fairy",
+        "color":"#F1B0F1"
+    },
+   {
+        "type":"Flying",
+        "color":"#778DE1"
+   },
+   {
+        "type":"Ground",
+        "color":"#D4B359"
+   },
+   {
+        "type":"Poison",
+        "color":"#8A498C"
+   },
+   {
+        "type":"Steel",
+        "color":"#3D3E48"
+   },
+   {
+        "type":"Dragon",
+        "color":"#7A63E4"
+   },
+   {
+        "type":"Fighting",
+        "color":"#713121"
+   },
+   {
+        "type":"Ghost",
+        "color":"#5F5FB1"
+   },
+   {
+        "type":"Ice",
+        "color":"#74D5F6"
+   },
+   {
+        "type":"Psychic",
+        "color":"#E54377"
+   },
+   {
+        "type":"Water",
+        "color":"#1666B4"
+   },
+
+]
 
 
+export default fakemon_type;
 export default Link;
--- script/main.js
+++ script/main.js
@@ -1,6 +1,16 @@
 import fetchFunction from "./fetch.js";
-
+import fakemon_type from "./const.js"
 let fakemon_species = document.getElementById("fakemon_species");
 
 document.querySelector("#genrateRandomSpecies").addEventListener("click", event => fetchFunction.generateSpecies(event,fakemon_species));
 
+function getRandomType(event){
+    const typeNumber = Math.round(Math.random()*2);
+    for(let idx=1 ; idx <= typeNumber ; idx++ ){
+        const type = fakemon_type[Math.floor((Math.random()*fakemon_type.length))];
+        const fakemon_type_html = document.getElementById("fakemon_type_"+idx);
+        fakemon_type.innerHTML = type.type;
+        fakemon_type_html.style.color = type.color;
+        
+    }
+}
