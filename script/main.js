c296bea58fc7aeebf610cd83eb39429a8d3f451b
thomastruffin <95939163+thomastruffin@users.noreply.github.com>
2026-07-03 12:35:30 +0000

    Correction

--- script/main.js
+++ script/main.js
@@ -9,7 +9,7 @@ function getRandomType(event){
     event.preventDefault();
     const typeNumber = Math.round(Math.random()*2);
     for(let idx=1 ; idx <= typeNumber ; idx++ ){
-        const type = FakemonType[Math.floor((Math.random()*fakemon_type.length))];
+        const type = FakemonType[Math.floor((Math.random()*FakemonType.length))];
         const fakemon_type_html = document.getElementById("fakemon_type_"+idx);
         fakemon_type.innerHTML = type.type;
         fakemon_type_html.style.color = type.color;
