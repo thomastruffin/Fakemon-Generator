2dbf45ef1298ee9cc7d17a2d507a43396abf2184
thomastruffin <95939163+thomastruffin@users.noreply.github.com>
2026-07-03 11:19:40 +0000

    supprimer Debug

--- script/main.js
+++ script/main.js
@@ -1,19 +1,15 @@
 import Link from "./const.js"
 function generateSpecies(event){
     event.preventDefault;
-    console.log("entree fonction");
     fetch(Link.animal_API).then(response => 
     {
-        console.log(response);
         if (!response.ok) {
             throw new Error(`Statut de réponse : ${reponse.status}`);
         }
-        console.log(response.json());
         return response.json();
     }
 ).then(json =>
 {   
-    console.log(json);
     const speciesname = json.city;
     return document.getElementById("fakemon_species").innerHTML = speciesname;
 }
