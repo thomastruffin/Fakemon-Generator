428289bc9a5d9181bea5a6108538640f523de638
thomastruffin <95939163+thomastruffin@users.noreply.github.com>
2026-07-03 11:44:59 +0000

    factoring code

--- script/fetch.js
+++ script/fetch.js
@@ -22,3 +22,26 @@ function generateSpecies(event, innerHTML){
     throw error;
 })
 }
+
+const fetch = {
+    getMyth : function(){},
+    generateSpecies:  function(event, innerHTML){
+    event.preventDefault;
+    fetch(Link.animal_API).then(response => 
+    {
+        if (!response.ok) {
+            throw new Error(`Statut de réponse : ${reponse.status}`);
+        }
+        return response.json();
+    }
+).then(json =>{
+    innerHTML.innerHTML = json.city;
+    return json.city;
+
+}
+).catch(error => {
+    throw error;
+})
+}
+}
+export default fetch
--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
-
+import fetch from "./fetch.js";
 
 let fakemon_species = document.getElementById("fakemon_species");
 
-document.querySelector("#genrateRandomSpecies").addEventListener("click", event => {generateSpecies(event,fakemon_species)});
+document.querySelector("#genrateRandomSpecies").addEventListener("click", event => {fetch.generateSpecies(event,fakemon_species)});
