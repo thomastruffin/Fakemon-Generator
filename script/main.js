3222c1c8e0605da6056fc30bae3aa2f34b066130
thomastruffin <95939163+thomastruffin@users.noreply.github.com>
2026-07-03 11:46:05 +0000

    code; git push aled

--- script/main.js
+++ script/main.js
@@ -2,4 +2,5 @@ import fetch from "./fetch.js";
 
 let fakemon_species = document.getElementById("fakemon_species");
 
-document.querySelector("#genrateRandomSpecies").addEventListener("click", event => {fetch.generateSpecies(event,fakemon_species)});
+document.querySelector("#genrateRandomSpecies").addEventListener("click", event => fetch.generateSpecies(event,fakemon_species));
+
