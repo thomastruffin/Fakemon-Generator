3525d9857afbf5e503c79d2ae1602d37ea4ec749
thomastruffin <95939163+thomastruffin@users.noreply.github.com>
2026-07-03 12:27:03 +0000

    html_change

--- script/main.js
+++ script/main.js
@@ -3,8 +3,10 @@ import fakemon_type from "./const.js"
 let fakemon_species = document.getElementById("fakemon_species");
 
 document.querySelector("#genrateRandomSpecies").addEventListener("click", event => fetchFunction.generateSpecies(event,fakemon_species));
+document.querySelector("#generateType").addEventListener("click", event => getRandomType(event));
 
 function getRandomType(event){
+    event.preventDefault();
     const typeNumber = Math.round(Math.random()*2);
     for(let idx=1 ; idx <= typeNumber ; idx++ ){
         const type = fakemon_type[Math.floor((Math.random()*fakemon_type.length))];
